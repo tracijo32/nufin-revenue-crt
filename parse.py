e27dc208662fb46d2a45ae53373651a491de64e2
Traci Johnson <traci.johnson.32@gmail.com>
2026-09-04 16:16:23 -0500

    small changes

--- parse.py
+++ parse.py
@@ -80,6 +80,7 @@ def parse_membership_report(path: os.PathLike):
         date_cols=['payment_date','created_date'],
         num_cols=['amount','payment_credit']
     )
+    df['event_name'] = 'Membership Payment'
     df['chart_string'] = format_chart_string(df['chart_string'])
     df['action_id'] = df['payment_date'].dt.strftime('%Y%m%d') + '-' + \
         df['donor_id'] + '-' + \
@@ -148,8 +149,6 @@ def parse_stripe_report(
     df[numeric_columns] = df[numeric_columns].apply(pd.to_numeric, errors='coerce').astype(float)
     df['created'] = pd.to_datetime(df['created'], errors='coerce')\
         .dt.tz_localize('UTC').dt.tz_convert('America/Chicago')
-    df['type'] = pd.Categorical(df['type'].str.lower().str.strip().str.replace(' ','_'),
-        categories=['charge','refund','stripe_fee'])
 
     assert df['currency'].eq('usd').all(), 'Currency is not USD'
     assert df['converted_currency'].eq('usd').all(), 'Converted currency is not USD'
@@ -179,100 +178,3 @@ def parse_stripe_report(
     
     return df
 
-def parse_chart_string_overrides(file_path: str):
-    df = pd.read_csv(file_path,dtype=str)\
-        .reindex(columns=['chart_string','event_name','item_name',
-        'description','fee_assignment','item_amount'])
-
-    assert df['chart_string'].notnull().all(), 'chart string cannot be null in override file'
-    df['item_amount'] = df['item_amount'].astype(float)
-    df = df.drop_duplicates()
-    return df
-
-def assign_gateway(
-    invoice_df: pd.DataFrame,
-    gateway_map: dict,
-    default_gateway: str = 'ARD'
-):
-    invoice_df['gateway'] = invoice_df['gateway_name'].map(gateway_map)\
-        .fillna(default_gateway)
-    return invoice_df
-
-def apply_chart_string_overrides(
-    invoice_df: pd.DataFrame, 
-    item_df: pd.DataFrame, 
-    cs_ovrd_df: pd.DataFrame
-):
-    cs_df = pd.merge(
-        item_df.loc[
-            item_df['total'].fillna(0).gt(0) &
-            item_df['chart_string'].notna(),
-                ['invoice_id','item_name','chart_string']
-        ],
-        invoice_df[['invoice_id','event_name','gateway']],
-        on=['invoice_id'],
-        how='inner'
-    ).drop_duplicates()
-
-    cs_df = pd.merge(
-        cs_df,
-        cs_ovrd_df,
-        on=['event_name'],
-        how='left',
-        suffixes=('','_remap')
-    )
-
-    cond1 = cs_df['item_name'].eq(cs_df['item_name_remap']) & cs_df['chart_string_remap'].notna()
-    cond2 = cs_df['item_name_remap'].isna() & cs_df['chart_string_remap'].notna()
-    cond3  = cs_df['chart_string_remap'].isna()
-
-    cs_df = cs_df[cond1 | cond2 | cond3]
-    cs_df['chart_string'] = cs_df['chart_string'].fillna(cs_df['chart_string_remap'])
-
-    cs_df = cs_df[['invoice_id','chart_string','item_name','gateway']].drop_duplicates()
-
-    cs_df['fund'] = cs_df['chart_string'].str.split('-').str[0]
-    cs_df.loc[cs_df['fund'].str.len().ne(3),'fund'] = None
-
-    cs_df['dept'] = cs_df['chart_string'].str.split('-').str[1]
-    cs_df.loc[cs_df['dept'].str.len().ne(7),'dept'] = None
-
-    cs_df['project'] = cs_df['chart_string'].str.split('-').str[2]
-    cs_df.loc[cs_df['project'].str.len().ne(8),'project'] = None
-
-    cs_df['activity'] = cs_df['project'].str.replace(r'\d+','01',regex=True)
-
-    cs_df['cf1'] = cs_df['chart_string'].str.split('-').apply(
-        lambda x: [s for s in x if len(s) == 4]
-    ).apply(lambda x: None if len(x) == 0 else x[0])
-    cs_df.loc[cs_df['cf1'].str.len().ne(4),'cf1'] = None
-
-    cs_df['account'] = cs_df['chart_string'].str.split('-').str[-1]
-    cs_df.loc[cs_df['account'].str.len().ne(5),'account'] = None
-
-    cs_df['account'] = cs_df['account'].fillna(
-        cs_df['gateway'].map(
-            {
-                'ARD': '40756',
-                'FSM': '40755'
-            }
-        )
-    )
-
-    cs_df['chart_string'] = cs_df[['fund','dept','project','activity','cf1','account']].apply(
-        lambda x: "-".join(x.dropna().astype(str)),axis=1
-    )
-
-    cs_df = cs_df[['invoice_id','item_name','chart_string']].drop_duplicates()
-
-    item_df = pd.merge(
-        item_df, cs_df,
-        on=['invoice_id','item_name'],
-        how='left',
-        suffixes=('_orig','')
-    )
-
-    item_df['chart_string'] = item_df['chart_string'].fillna(item_df['chart_string_orig'])
-    item_df = item_df.drop(columns=['chart_string_orig'])
-
-    return item_df
