9ccc55cb39f85af870053f46e9b6e63e9923fbcb
Traci Johnson <traci.johnson.32@gmail.com>
2026-09-09 15:25:48 -0500

    added code to aggregate all the way to CRT lines

--- aggregate.py
+++ aggregate.py
@@ -1,6 +1,6 @@
 import pandas as pd
 from config import Config
-from data import BlackthornData
+from data import BlackthornData, StripeData
 
 def aggregate_blackthorn(
     matched_blackthorn_df: pd.DataFrame,
@@ -19,7 +19,7 @@ def aggregate_blackthorn(
     ).rename(columns={'amount_blackthorn':'amount','fees_blackthorn':'fees'})
 
     fee_assign = config.load_fee_assignment_by_event()
-    fees = pd.merge(
+    df = pd.merge(
         df,
         fee_assign,
         on='event_name',
@@ -27,23 +27,36 @@ def aggregate_blackthorn(
     )
 
     default_cs = config.default_stripe_fee_chart_string
-    fees['assignment'] = fees['school'].fillna(fees['gateway'])
-    fees['chart_string'] = fees['chart_string'].fillna(default_cs)
+    df['assignment'] = df['school'].fillna(df['gateway'])
+    df['chart_string'] = df['chart_string'].fillna(default_cs)
 
-    fees = fees.groupby(['gateway','wire_date','assignment','chart_string'])\
-        ['fees'].sum().reset_index()
+    fees = df.groupby(['gateway','wire_date','assignment','chart_string'])\
+        ['fees'].sum().abs().multiply(-1)\
+            .reset_index()
+    fees['description'] = fees['assignment'].apply(
+        lambda x: f'Stripe Transaction Fees - {x}'
+    )
+    fees = fees.drop(columns=['assignment'])
 
     df = pd.merge(
-        df,
+        df.drop(columns=['chart_string','school']),
         item_df[['invoice_id','chart_string','total']],
         on='invoice_id'
     )
 
     gross = df[df['total'].fillna(0).gt(0)]\
-        .groupby(['gateway','wire_date','chart_string'])\
-        ['total'].sum()\
-        .reset_index()\
-        .rename(columns={'total':'gross'})
+        .rename(columns={'total':'gross'})\
+        .groupby(['gateway','wire_date','assignment','chart_string'])\
+        ['gross'].sum().abs()\
+            .reset_index()
+
+    chart_string_desc = config.load_chart_string_descriptions()
+    gross['desc1'] = gross['chart_string'].map(chart_string_desc)
+    gross['desc2'] = gross['assignment'].apply(
+        lambda x: f'Event Revenue - {x}'
+    )
+    gross['description'] = gross['desc1'].fillna(gross['desc2'])
+    gross = gross.drop(columns=['desc1','desc2','assignment'])
 
     return gross, fees
 
@@ -53,6 +66,7 @@ def aggregate_memberships(
 ):
     default_fee_chart_string = config.default_stripe_fee_chart_string
     fee_assign = config.load_fee_assignment_by_event()
+    chart_string_desc = config.load_chart_string_descriptions()
 
     fees = pd.merge(
         matched_memberships_df[[
@@ -69,11 +83,110 @@ def aggregate_memberships(
     fees['chart_string'] = fees['chart_string'].fillna(default_fee_chart_string)
 
     fees = fees.groupby(['gateway','wire_date','assignment','chart_string'])\
-        ['fees'].sum().reset_index()
+        ['fees'].sum().abs().multiply(-1)\
+            .reset_index()
+    fees['description'] = fees['assignment'].apply(
+        lambda x: f'Stripe Transaction Fees - {x}'
+    )
+    fees = fees.drop(columns=['assignment'])
 
     gross = matched_memberships_df\
-        .groupby(['gateway','wire_date','chart_string'])['amount'].sum()\
-        .reset_index().rename(columns={'amount':'gross'})
+        .rename(columns={'amount':'gross'})\
+        .groupby(['gateway','wire_date','chart_string'])['gross'].sum().abs()\
+            .reset_index()
+
+    chart_string_desc = config.load_chart_string_descriptions()
+    default_cs_desc = config.default_membership_chart_string_description
+    gross['description'] = gross['chart_string'].map(chart_string_desc)\
+        .fillna(default_cs_desc)
 
     return gross, fees
 
+def aggregate_usage_fees(
+    stripe_data: StripeData,
+    config: Config
+):
+    df = stripe_data.transactions.loc[
+        stripe_data.transactions['type'].eq('Stripe Fee'),
+        ['gateway','wire_date','amount']
+    ].groupby(['gateway','wire_date'])['amount'].sum()\
+        .reset_index()
+    df['chart_string'] = config.default_stripe_fee_chart_string
+    df['description'] = 'Stripe Usage Fees'
+    return df
+
+def balance_charges(
+    stripe_data: StripeData,
+    gross_df: pd.DataFrame,
+    fees_df: pd.DataFrame
+):
+    cc_totals = pd.merge(
+        gross_df.groupby(['gateway','wire_date'])['gross'].sum(),
+        fees_df.groupby(['gateway','wire_date'])['fees'].sum(),
+        left_index=True,
+        right_index=True,
+        how='outer'
+    ).fillna(0)
+
+    stripe_totals = stripe_data.transactions.loc[
+        stripe_data.transactions['type'].eq('Charge'),
+        ['gateway','wire_date','amount','fees']
+    ].groupby(['gateway','wire_date'])[['amount','fees']].sum()\
+        .rename(columns={'amount':'gross'})
+    stripe_totals['gross'] = stripe_totals['gross'].abs()
+    stripe_totals['fees'] = stripe_totals['fees'].abs().multiply(-1)
+
+    df = pd.merge(
+        stripe_totals,
+        cc_totals,
+        left_index=True,
+        right_index=True,
+        how='left',
+        suffixes=('_stripe','_cc')
+    ).fillna(0)
+
+    df['gross_diff'] = df['gross_cc'].subtract(df['gross_stripe']).round(2)
+    df['fees_diff'] = df['fees_cc'].subtract(df['fees_stripe']).round(2)
+
+    df['balanced'] = df['gross_diff'].eq(0) & df['fees_diff'].eq(0)
+
+    return df
+
+def balance_refunds(
+    stripe_data: StripeData,
+    refund_df: pd.DataFrame
+):
+    cc_totals = refund_df.groupby(['gateway','wire_date'])['amount'].sum()
+    stripe_totals = stripe_data.transactions.loc[
+        stripe_data.transactions['type'].eq('Refund'),
+        ['gateway','wire_date','amount']
+    ].groupby(['gateway','wire_date'])['amount'].sum()
+
+    df = pd.merge(
+        stripe_totals,
+        cc_totals,
+        left_index=True,
+        right_index=True,
+        how='left',
+        suffixes=('_stripe','_cc')
+    ).fillna(0)
+
+    df['amount_diff'] = df['amount_cc'].subtract(df['amount_stripe']).round(2)
+    df['balanced'] = df['amount_diff'].eq(0)
+
+    return df
+
+def get_crt_lines(
+    gross_df: pd.DataFrame,
+    fees_df: pd.DataFrame,
+    usage_df: pd.DataFrame
+):
+    
+    df = pd.concat([
+        gross_df.rename(columns={'gross':'amount'}),
+        fees_df.rename(columns={'fees':'amount'}),
+        usage_df.rename(columns={'amount':'amount'})
+    ]).groupby(['gateway','wire_date','chart_string','description'])\
+        ['amount'].sum().reset_index()
+
+    return df
