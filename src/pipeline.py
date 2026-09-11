776871969af4867fac7daddd8b83b6a17d361b52
Traci Johnson <traci.johnson.32@gmail.com>
2026-09-11 11:51:09 -0500

    added print statements so you actually know the code is running now

--- src/pipeline.py
+++ src/pipeline.py
@@ -4,28 +4,39 @@ import match
 import aggregate as agg
 import pandas as pd
 import os
+from tqdm import tqdm
 from output import generate_daily_gateway_output_file
 
 def run_pipeline(
     config: Config
 ) -> tuple[BlackthornData, MembershipData, StripeData]:
 
+    print('Running daily reconciliation pipeline')
+
     ## load in the transaction data from each source
+    print('Loading blackthorn data...', end='')
     bt_data = config.load_blackthorn_data()
+    print('done')
+    print('Loading membership data...', end='')
     mbr_data = config.load_membership_data()
+    print('done')
+    print('Loading stripe data...', end='')
     stripe_data = config.load_stripe_data()
+    print('done')
 
     ## dump the raw data to a csv, so you can see what it looks like unprocessed
     data_path = os.path.join(config.output_path,'data')
     os.makedirs(data_path,exist_ok=True)
+    print('Dumping raw data to csv...', end='')
     bt_data.dump_data(os.path.join(data_path,'raw_blackthorn_data.csv'))
     mbr_data.dump_data(os.path.join(data_path,'raw_membership_data.csv'))
     stripe_data.dump_data(os.path.join(data_path,'stripe_data.csv'))
+    print('done, files saved to', data_path)
 
     #########################################################
     ### clean the data
     #########################################################
-
+    print('Cleaning data and applying overrides....', end='')
     ## load in the chart string mapping and apply it to the data
     full_cs_map_df = match.complete_chart_string_mapping(
         blackthorn_data=bt_data,
@@ -41,14 +52,18 @@ def run_pipeline(
 
     ## apply the line item overrides to the data
     bt_data.apply_line_item_overrides(config.load_line_item_override())
+    print('done')
 
+    print('Dumping processed data to csv...', end='')
     ## dump the processed data to a csv, so you can see what it looks like processed
     bt_data.dump_data(os.path.join(data_path,'processed_blackthorn_data.csv'))
     mbr_data.dump_data(os.path.join(data_path,'processed_membership_data.csv'))
+    print('done, files saved to', data_path)
 
     #########################################################
     ### match the transactions
     #########################################################
+    print('Matching blackthorn and membership transactions to stripe transactions...', end='')
     refund_df = match.match_refunds(bt_data, stripe_data, config)
 
     ## dump the matched data to a csv, so you can see what it looks like matched
@@ -67,11 +82,12 @@ def run_pipeline(
         index=False,
         date_format='%Y-%m-%d'
     )
+    print('done, matched files saved to', config.output_path)
 
     #########################################################
     ### aggregate the data
     #########################################################
-
+    print('Aggregating data...', end='')
     ## aggregate the blackthorn data
     bt_gross, bt_fees = agg.aggregate_blackthorn(bt_match_df, bt_data, config)
 
@@ -98,7 +114,7 @@ def run_pipeline(
 
     ## aggregate the usage fees
     usage_df = agg.aggregate_usage_fees(stripe_data, config)
-
+    print('done')
     #########################################################
     ## get the crt lines
     crt_lines = agg.get_crt_lines(gross_df, fees_df, refund_df, usage_df)
@@ -115,7 +131,10 @@ def run_pipeline(
 
     ## output the daily reconciliation files
     daily = crt_bal[['gateway','wire_date']].drop_duplicates()
-    for gateway, wire_date in daily.itertuples(index=False):
+    for gateway, wire_date in tqdm(
+        daily.itertuples(index=False), 
+        total=len(daily), 
+        desc='Generating daily reconciliation files'):
         generate_daily_gateway_output_file(
             wire_date=wire_date,
             gateway=gateway,
@@ -124,5 +143,7 @@ def run_pipeline(
             crt_bal=crt_bal,
             mbr_match_df=mbr_match_df,
             bt_match_df=bt_match_df,
-            bt_data=bt_data
+            bt_data=bt_data,
+            refund_df=refund_df
         )
+    print('Reconciliation complete. Daily files saved to', daily_path)
